78abf56b85413d9b4d356c034a984c3d5a450e8a
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-07-05 06:49:43 +0100

    took the login route above to root.

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { HashRouter as Router } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import Login from './Login';
 
 import App from './App'
 import theme from '../config/Theme'
@@ -11,7 +13,10 @@ const muiTheme = getMuiTheme(theme)
 const Root = () => (
   <Router>
     <MuiThemeProvider muiTheme={muiTheme}>
-      <App />
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route path="/" component={App} />
+      </Switch>
     </MuiThemeProvider>
   </Router>
 )
